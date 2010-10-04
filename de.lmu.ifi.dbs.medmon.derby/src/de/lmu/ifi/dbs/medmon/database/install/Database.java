8b0a20c9ca6feb58d061cbaf21904c7d41b97a24
muki <muki@ba423995-4c20-4507-9716-2cce29bf152e>
2010-10-04 17:00:32 +0000

    git-svn-id: https://svn.cip.ifi.lmu.de/~seilern/svn/eclipse_medmon/trunk@47 ba423995-4c20-4507-9716-2cce29bf152e

--- de.lmu.ifi.dbs.medmon.derby/src/de/lmu/ifi/dbs/medmon/database/install/Database.java
+++ de.lmu.ifi.dbs.medmon.derby/src/de/lmu/ifi/dbs/medmon/database/install/Database.java
@@ -6,47 +6,56 @@ import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.eclipse.persistence.config.PersistenceUnitProperties;
-import org.eclipse.persistence.jpa.PersistenceProvider;
 
 import de.lmu.ifi.dbs.medmon.database.model.Patient;
 
 public class Database {
 
-	
 	private HashMap<String, Object> properties;
+	private EntityManagerFactory emf;
+	private EntityManager entityManager;
 
 	public Database() {
-		PersistenceProvider provider = new PersistenceProvider();
-		
 		properties = new HashMap<String, Object>();
 		String home = System.getProperty("user.home");
-		String sep  = System.getProperty("file.separator");
-		String url  = "jdbc:derby:" + home + sep + ".medmon" + sep + "db;create=true";
+		String sep = System.getProperty("file.separator");
+		String url = "jdbc:derby:" + home + sep + ".medmon" + sep
+				+ "db;create=true";
 		System.out.println(url);
 		properties.put(PersistenceUnitProperties.JDBC_URL, url);
-		
+
 		System.out.println(new File(url).mkdirs());
-		init(provider);
+		connect();
+		test();
+	}
+
+	private void connect() {
+		emf = Persistence.createEntityManagerFactory("medmon", properties);
+		entityManager = emf.createEntityManager();
 
 	}
 	
-	private void init(PersistenceProvider provider) {
-		EntityManagerFactory emf =  provider.createEntityManagerFactory("medmon", null);
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		Patient p = new Patient();
-		p.setFirstname("Muki");
-		p.setLastname("Seiler");
-		em.persist(p);
-		em.getTransaction().commit();
+	private void test() {
+		entityManager.getTransaction().begin();
+		Patient patient = new Patient("Muki", "Seiler");
+		entityManager.persist(patient);
+		entityManager.getTransaction().commit();
+		System.out.println("New patient: " + patient + " with id: " + patient.getId());
+		
+		entityManager.getTransaction().begin();
+		Patient muki = entityManager.find(Patient.class, patient.getId());
+		System.out.println("Muki: " + muki);
+		entityManager.remove(muki);
+		entityManager.getTransaction().commit();
 	}
-	
+
 	public Map<String, Object> getProperties() {
 		return properties;
 	}
-	
+
 	public Object getProperty(String key) {
 		return properties.get(key);
 	}
