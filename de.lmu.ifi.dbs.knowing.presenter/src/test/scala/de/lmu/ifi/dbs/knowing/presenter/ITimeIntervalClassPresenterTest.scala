c47687fbfcdd5a933c23d28075e9dcbd6c397a7a
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2012-01-13 10:40:42 +0100

    Kommentar

--- de.lmu.ifi.dbs.knowing.presenter/src/test/scala/de/lmu/ifi/dbs/knowing/presenter/ITimeIntervalClassPresenterTest.scala
+++ de.lmu.ifi.dbs.knowing.presenter/src/test/scala/de/lmu/ifi/dbs/knowing/presenter/ITimeIntervalClassPresenterTest.scala
@@ -2,31 +2,58 @@ package de.lmu.ifi.dbs.knowing.presenter
 
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FunSuite
-import org.scalatest.BeforeAndAfterAll
+import org.scalatest.BeforeAndAfter
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
-import weka.core.{ Attribute, Instances }
+import weka.core.{ Attribute, Instances, DenseInstance }
 import weka.core.Attribute.{ NUMERIC, NOMINAL, DATE, RELATIONAL }
 import java.util.Date
 import akka.testkit.TestKit
+import akka.actor.Actor.actorOf
+import akka.actor.ActorRef
+
+import de.lmu.ifi.dbs.knowing.core.util.ResultsUtil._
+import de.lmu.ifi.dbs.knowing.core.events._
 
 /**
  * Take a look at to finish implementation.
- * 
+ *
  * @see http://akka.io/docs/akka/1.2/scala/testkit-example.html
  * @see http://akka.io/api/akka/1.2/akka/testkit/TestKit.html
  */
 @RunWith(classOf[JUnitRunner])
-class ITimeIntervalClassPresenterTest extends FunSuite with ShouldMatchers with BeforeAndAfterAll with TestKit {
+class ITimeIntervalClassPresenterTest extends FunSuite with ShouldMatchers with BeforeAndAfter with TestKit {
 
-  var presenter: ITimeIntervalClassPresenter[_] = _
+  var presenter: ActorRef = _
   var testInstances: Instances = _
 
-  override def beforeAll(configMap: Map[String, Any]) {
+  before {
+    presenter = actorOf[TestTimeIntervalClassPresenter].start
+    testInstances = timeIntervalResult(List("A", "B", "C"))
+
   }
 
-  override def afterAll() {
-    
+  after {
+    presenter.stop
+  }
+
+  test("Check Presenter") {
+    for (i <- 0 to 5) {
+      val clazz = i % 3
+      val from = i * 1000
+      val to = from + 100
+      val inst = new DenseInstance(1, Array(clazz, from, to))
+      testInstances.add(inst)
+    }
+
+    testInstances.size should equal(6)
+
+    presenter ! Results(testInstances)
+
+
+    expectMsg {
+      case msg: Array[String] => Array()
+    }
   }
 
 }
