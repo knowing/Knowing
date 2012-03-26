f9e7bfcc7dc739397e60ae15d82ff309679d25d9
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2012-03-26 10:44:40 +0200

    Adding Knowing logo to sources in * knowing.core.swt * knowing.core.swt.charts

--- de.lmu.ifi.dbs.knowing.weka/src/main/scala/de/lmu/ifi/dbs/knowing/weka/classifier/WekaNaiveBayes.scala
+++ de.lmu.ifi.dbs.knowing.weka/src/main/scala/de/lmu/ifi/dbs/knowing/weka/classifier/WekaNaiveBayes.scala
@@ -1,13 +1,13 @@
-/*																*\
-** |Â¯Â¯|/Â¯Â¯/|Â¯Â¯ \|Â¯Â¯| /Â¯Â¯/\Â¯Â¯\'|Â¯Â¯|  |Â¯Â¯||Â¯Â¯||Â¯Â¯ \|Â¯Â¯| /Â¯Â¯/|__|	**
-** | '| '( | '|\  '||  |  | '|| '|/\| '|| '|| '|\  '||  | ,---,	**
-** |__|\__\|__|'|__| \__\/__/'|__,/\'__||__||__|'|__| \__\/__|	**
-** 																**
-** Knowing Framework											**
-** Apache License - http://www.apache.org/licenses/				**
-** LMU Munich - Database Systems Group							**
-** http://www.dbs.ifi.lmu.de/									**
-\*																*/
+/*                                                              *\
+** |¯¯|/¯¯/|¯¯ \|¯¯| /¯¯/\¯¯\'|¯¯|  |¯¯||¯¯||¯¯ \|¯¯| /¯¯/|__|  **
+** | '| '( | '|\  '||  |  | '|| '|/\| '|| '|| '|\  '||  | ,---, **
+** |__|\__\|__|'|__| \__\/__/'|__,/\'__||__||__|'|__| \__\/__|  **
+**                                                              **
+** Knowing Framework                                            **
+** Apache License - http://www.apache.org/licenses/             **
+** LMU Munich - Database Systems Group                          **
+** http://www.dbs.ifi.lmu.de/                                   **
+\*                                                              */
 package de.lmu.ifi.dbs.knowing.weka.classifier
 
 import de.lmu.ifi.dbs.knowing.core.factory._
