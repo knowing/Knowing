cac0e89fab113882a777b7ccf0b6231b048c9be0
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2012-04-22 21:43:11 +0200

    Little fixes * Corrected version numbers * Extended reference.conf * Created debug.launcher

--- de.lmu.ifi.dbs.knowing.debug.launcher/src/de/lmu/ifi/dbs/knowing/debug/launcher/Activator.scala
+++ de.lmu.ifi.dbs.knowing.debug.launcher/src/de/lmu/ifi/dbs/knowing/debug/launcher/Activator.scala
@@ -8,7 +8,7 @@
  ** LMU Munich - Database Systems Group                          **
  ** http://www.dbs.ifi.lmu.de/                                   **
 \*                                                               */
-package de.lmu.ifi.dbs.knowing.launcher.internal
+package de.lmu.ifi.dbs.knowing.debug.launcher
 
 import org.osgi.framework.{ BundleContext, BundleActivator }
 import de.lmu.ifi.dbs.knowing.launcher.LaunchConfiguration
@@ -18,31 +18,26 @@ import de.lmu.ifi.dbs.knowing.core.exceptions.ValidationException
 import com.typesafe.config.ConfigFactory
 import java.net.URI
 import java.nio.file.Paths
+import org.slf4j.LoggerFactory
+
 
 /**
- * To launch an DPU with the launcher:
- * 
- * LaunchConfiguration.APPLICATION_CONF (knowing.launcher.configuration) => path to application.conf 
- * 
- * Command could look like: 
- * <code>launch /home/user/knowing/naiveBayes/application.conf</code>
  *
  * @author Nepomuk Seiler
  * @version 0.1
  * @since 2012-04-19
  */
 class Activator extends BundleActivator {
+	
+	val log = LoggerFactory.getLogger(Activator.PLUGIN_ID)
 
-	/**
-	 * The evaluation starts, when the bundle is activated.
-	 */
 	def start(context: BundleContext) = {
+
 		val configUriString = System.getProperty(LaunchConfiguration.APPLICATION_CONF)
 
 		if (configUriString != null && configUriString.nonEmpty) {
 			val configUrl = new URI(configUriString).toURL
 			val config = ConfigFactory.parseURL(configUrl)
-			println(config.root.render)
 
 			val launchConfig = new LaunchConfiguration(config)
 			val dpu = launchConfig.dpu
@@ -50,14 +45,22 @@ class Activator extends BundleActivator {
 			if (reference.isDefined) {
 				try {
 					val evaluateService = context.getService(reference.get)
-					//TODO Evaluate
+					evaluateService.evaluate(dpu, Paths.get(System.getProperty("user.home")).toUri)
 				} catch {
 					case e: ValidationException => System.err.println(e.getErrors());
 				}
+
 			}
+
 		}
 	}
 
-	def stop(context: BundleContext) = {}
+	def stop(context: BundleContext) = {
 
+	}
+
+}
+
+object Activator {
+	val PLUGIN_ID = "de.lmu.ifi.dbs.knowing.debug.launcher"
 }
