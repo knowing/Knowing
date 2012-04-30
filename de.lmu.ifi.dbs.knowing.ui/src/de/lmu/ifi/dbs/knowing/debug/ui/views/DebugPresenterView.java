cc00f575c5a1c0276ee6a0da17da8ba8ca8a37e9
muki <muki@Slave1>
2012-04-30 23:58:43 +0200

    Adding Google Guava dependency. Splitting .progress file

--- de.lmu.ifi.dbs.knowing.ui/src/de/lmu/ifi/dbs/knowing/debug/ui/views/DebugPresenterView.java
+++ de.lmu.ifi.dbs.knowing.ui/src/de/lmu/ifi/dbs/knowing/debug/ui/views/DebugPresenterView.java
@@ -22,30 +22,34 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
-import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationType;
 import org.eclipse.debug.core.ILaunchesListener2;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 
+import com.google.common.base.Splitter;
+
 import de.lmu.ifi.dbs.knowing.debug.core.launching.DPULaunchConfigurationDelegate;
 import de.lmu.ifi.dbs.knowing.debug.presenter.DebugUIFactory;
+import de.lmu.ifi.dbs.knowing.debug.ui.interal.Activator;
 
 public class DebugPresenterView extends ViewPart implements ILaunchesListener2, UncaughtExceptionHandler {
-	public DebugPresenterView() {
-	}
 
 	public static final String	ID	= "de.lmu.ifi.dbs.knowing.debug.ui.views.DebugPresenterView";	//$NON-NLS-1$
 	private Text				txtConsole;
+
+	private ILaunch				currentLaunch;
 
 	/**
 	 * Create contents of the view part.
@@ -80,35 +84,49 @@ public class DebugPresenterView extends ViewPart implements ILaunchesListener2, 
 	@Override
 	public void launchesChanged(ILaunch[] launches) {
 		try {
-			// TODO check launch
+			if (launches.length == 0)
+				return;
+
 			ILaunch launch = launches[0];
 
 			// Only interested in DPU launch configurations
 			ILaunchConfigurationType type = launch.getLaunchConfiguration().getType();
-			
+
 			if (!DPULaunchConfigurationDelegate.LAUNCH_TYPE_ID.equals(type.getIdentifier()))
 				return;
+
+			if (currentLaunch != null) {
+				MessageDialog.openError(getSite().getShell(), "Another DPU is currently running", "Stop the other instance running.");
+				// Ask what to do. Stop current or new launch
+				launch.terminate();
+				return;
+			}
+
 			String executionPath = launch.getLaunchConfiguration().getAttribute(DPULaunchConfigurationDelegate.DPU_EXECUTION_PATH, "");
 			ExecutionWatcherRunnable watcherRunnable = new ExecutionWatcherRunnable(executionPath);
 			Thread thread = new Thread(watcherRunnable);
 			thread.setName("ExecutionWatcherThread");
 			thread.start();
+			currentLaunch = launch;
 
 		} catch (CoreException | IOException e) {
 			e.printStackTrace();
+			ErrorDialog.openError(getSite().getShell(), "Error while launching DPU", e.getMessage(), new Status(IStatus.ERROR,
+					Activator.PLUGIN_ID, null, e));
 		}
 	}
 
 	@Override
 	public void launchesTerminated(ILaunch[] launches) {
-//		final Shell shell = getSite().getShell();
-//		shell.getDisplay().asyncExec(new Runnable() {
-//
-//			@Override
-//			public void run() {
-//				MessageDialog.openInformation(shell, "Execution finished", "Finished");
-//			}
-//		});
+		// final Shell shell = getSite().getShell();
+		// shell.getDisplay().asyncExec(new Runnable() {
+		//
+		// @Override
+		// public void run() {
+		// MessageDialog.openInformation(shell, "Execution finished",
+		// "Finished");
+		// }
+		// });
 
 	}
 
@@ -157,6 +175,8 @@ public class DebugPresenterView extends ViewPart implements ILaunchesListener2, 
 		private final Path				executionPath;
 		private AsynchronousFileChannel	progressChannel;
 		private long					position	= 0;
+
+		private String					lastLine;
 
 		ExecutionWatcherRunnable(String executionPath) throws IOException {
 			this.executionPath = Paths.get(executionPath);
@@ -210,8 +230,11 @@ public class DebugPresenterView extends ViewPart implements ILaunchesListener2, 
 
 					@Override
 					public void run() {
-						if (buffer.hasArray())
-							txtConsole.append(new String(buffer.array()));
+						if (buffer.hasArray()) {
+							String progressFile = new String(buffer.array());
+							txtConsole.append(progressFile);
+							showProgress(progressFile);
+						}
 					}
 				});
 			}
@@ -235,6 +258,38 @@ public class DebugPresenterView extends ViewPart implements ILaunchesListener2, 
 				return false;
 			}
 			return true;
+		}
+
+		private boolean showProgress(String progressFile) {
+			String trimmed = progressFile.trim(); //Remove zeros from buffer
+			Splitter splitter = Splitter.onPattern("\r?\n").trimResults().omitEmptyStrings(); //split on line break
+			Iterable<String> split = splitter.split(trimmed);
+			int i = 0;
+			for (String string : split) {
+				System.out.println("#[" + i + "," + string.isEmpty() + "] " + string );
+				i++;
+			}
+/*			if (status.length == 0)
+				return false;
+			if (status.length == 1) {
+				lastLine = lastLine + status[0];
+				if (lastLine.equals("Shutdown()"))
+					return true;
+				return false;
+			}
+			
+			String firstLine = lastLine + status[0];
+			System.err.println("First: " + firstLine);
+			for (int i = 1; i < status.length; i++) {
+				System.err.println("Status: " + status[i]);
+			}
+			
+			lastLine = status[status.length -1];
+			System.err.println("Last: " + lastLine);
+			if(lastLine.equals("Shutdown()"))
+				return true;*/
+			
+			return false;
 		}
 
 	}
