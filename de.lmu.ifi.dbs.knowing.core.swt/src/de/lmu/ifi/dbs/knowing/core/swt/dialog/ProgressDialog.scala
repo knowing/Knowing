311c7df22a54d5375bd1c3ce6a5e99def29f6c41
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2012-10-11 09:19:23 +0200

    FIX #42 - task 42: Knowing SWT: Details for Exception http://github.com/knowing/Knowing/issues/issue/42

--- de.lmu.ifi.dbs.knowing.core.swt/src/de/lmu/ifi/dbs/knowing/core/swt/dialog/ProgressDialog.scala
+++ de.lmu.ifi.dbs.knowing.core.swt/src/de/lmu/ifi/dbs/knowing/core/swt/dialog/ProgressDialog.scala
@@ -9,126 +9,128 @@ import org.eclipse.swt.custom.TableEditor
 import scala.collection.mutable.Map
 import akka.actor.{ ActorContext, ActorRef, ActorPath }
 import de.lmu.ifi.dbs.knowing.core.events._
-
+import org.eclipse.core.runtime.IStatus
+import org.eclipse.jface.dialogs.ErrorDialog
 
 class ProgressDialog(shell: Shell, var disposed: Boolean = false) extends Dialog(shell) {
 
-	import ProgressDialog._
+  import ProgressDialog._
 
-	var supervisorContext: ActorContext = _
+  var supervisorContext: ActorContext = _
 
-	private var table: Table = _
-	private var rows: Map[ActorPath, (TableItem, ProgressBar)] = Map()
+  private var table: Table = _
+  private var rows: Map[ActorPath, (TableItem, ProgressBar)] = Map()
 
-	setBlockOnOpen(false)
+  setBlockOnOpen(false)
 
-	/**
-	 * Create contents of the dialog.
-	 * @param parent
-	 */
-	override protected def createDialogArea(parent: Composite): Control = {
-		val container = super.createDialogArea(parent).asInstanceOf[Composite]
-		container.setLayout(new FillLayout(SWT.HORIZONTAL))
+  /**
+   * Create contents of the dialog.
+   * @param parent
+   */
+  override protected def createDialogArea(parent: Composite): Control = {
+    val container = super.createDialogArea(parent).asInstanceOf[Composite]
+    container.setLayout(new FillLayout(SWT.HORIZONTAL))
 
-		/* == Generate ProgressTabel == */
-		table = new Table(container, SWT.BORDER | SWT.FULL_SELECTION)
-		table.setHeaderVisible(true)
-		table.setLinesVisible(true)
-		cols foreach {
-			case (name, width) =>
-				val col = new TableColumn(table, SWT.NONE)
-				col.setText(name)
-				col.setWidth(width)
-		}
+    /* == Generate ProgressTabel == */
+    table = new Table(container, SWT.BORDER | SWT.FULL_SELECTION)
+    table.setHeaderVisible(true)
+    table.setLinesVisible(true)
+    cols foreach {
+      case (name, width) =>
+        val col = new TableColumn(table, SWT.NONE)
+        col.setText(name)
+        col.setWidth(width)
+    }
 
-		return container;
-	}
+    return container;
+  }
 
-	/**
-	 * Create contents of the button bar.
-	 * @param parent
-	 */
-	override protected def createButtonsForButtonBar(parent: Composite) {
-		//		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
-		//		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
-	}
+  /**
+   * Create contents of the button bar.
+   * @param parent
+   */
+  override protected def createButtonsForButtonBar(parent: Composite) {
+    //		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+    //		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
+  }
 
-	override protected def close: Boolean = {
-		if (supervisorContext != null) {
-			supervisorContext.stop(supervisorContext.self)
-		}
-		if (table != null)
-			table.dispose()
-		table = null
-		rows = Map()
-		supervisorContext = null
-		disposed = true
-		super.close
-	}
+  override protected def close: Boolean = {
+    if (supervisorContext != null) {
+      supervisorContext.stop(supervisorContext.self)
+    }
+    if (table != null)
+      table.dispose()
+    table = null
+    rows = Map()
+    supervisorContext = null
+    disposed = true
+    super.close
+  }
 
-	/**
-	 * Return the initial size of the dialog.
-	 */
-	override protected def getInitialSize: Point = new Point(450, 300)
+  /**
+   * Return the initial size of the dialog.
+   */
+  override protected def getInitialSize: Point = new Point(450, 300)
 
-	/**
-	 * Handles different status updates. Stops process on finish or exception.
-	 *
-	 * @param actor - the actor sending the message, non null
-	 * @param status - the status being send, non null
-	 */
-	def update(actor: ActorRef, status: Status) = {
-		status match {
+  /**
+   * Handles different status updates. Stops process on finish or exception.
+   *
+   * @param actor - the actor sending the message, non null
+   * @param status - the status being send, non null
+   */
+  def update(actor: ActorRef, status: Status) = {
+    status match {
 
-			//Actor created -> create ProgressBar
-			case Created() =>
-				val item = new TableItem(table, SWT.NONE)
-				val name = actor.path.name
-				item.setText(name)
-				val bar = new ProgressBar(table, SWT.NONE)
-				val editor = new TableEditor(table)
-				editor.grabHorizontal = true
-				editor.grabVertical = true
-				editor.setEditor(bar, item, 2)
-				rows += (actor.path -> (item, bar))
-				changed(actor, 0, "Created")
+      //Actor created -> create ProgressBar
+      case Created() =>
+        val item = new TableItem(table, SWT.NONE)
+        val name = actor.path.name
+        item.setText(name)
+        val bar = new ProgressBar(table, SWT.NONE)
+        val editor = new TableEditor(table)
+        editor.grabHorizontal = true
+        editor.grabVertical = true
+        editor.setEditor(bar, item, 2)
+        rows += (actor.path -> (item, bar))
+        changed(actor, 0, "Created")
 
-			//Actor makes progress -> update ProgressBar linked with this actor
-			case Progress(task, worked, work) =>
-				val row = rows(actor.path)
-				row._2.setMaximum(work)
-				val msg = task match {
-					case null | "" => "Progress"
-					case task => task
-				}
-				changed(actor, worked, msg)
+      //Actor makes progress -> update ProgressBar linked with this actor
+      case Progress(task, worked, work) =>
+        val row = rows(actor.path)
+        row._2.setMaximum(work)
+        val msg = task match {
+          case null | "" => "Progress"
+          case task => task
+        }
+        changed(actor, worked, msg)
 
-			//Should set ProgressBar to intermediate
-			case Running() => changed(actor, 0, "Running")
+      //Should set ProgressBar to intermediate
+      case Running() => changed(actor, 0, "Running")
 
-			//Close ProgressDialog
-			case Shutdown() => close
+      //Close ProgressDialog
+      case Shutdown() => close
 
-			//Handles exception thrown by actor
-			case ExceptionEvent(e, details) =>
-				MessageDialog.openError(new Shell, "Error in " + actor.path.name, details + " \n \n" + e.getMessage)
-				close
+      //Handles exception thrown by actor
+      case ExceptionEvent(e, details) =>
+        val status = new org.eclipse.core.runtime.Status(IStatus.ERROR, "No plugin", details, e)
+        ErrorDialog.openError(new Shell, "Error in " + actor.path.name, null, status)
+        close
 
-			case status => changed(actor, 0, status.toString)
-		}
-	}
+      case status => changed(actor, 0, status.toString)
+    }
+  }
 
-	/**
-	 * Status for actor changed
-	 */
-	private def changed(actor: ActorRef, selection: Int, msg: String) {
-		val row = rows(actor.path)
-		row._2.setSelection(selection)
-		row._1.setText(1, msg)
-	}
+  /**
+   * Status for actor changed
+   */
+  private def changed(actor: ActorRef, selection: Int, msg: String) {
+    val row = rows(actor.path)
+    row._2.setSelection(selection)
+    row._1.setText(1, msg)
+  }
 
 }
 
 object ProgressDialog {
-	val cols = Array(("Node", 150), ("Status", 100), ("Progress", 150))
+  val cols = Array(("Node", 150), ("Status", 100), ("Progress", 150))
 }
