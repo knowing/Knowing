1a6f6a1343cd7ca554a074a4035b53ab0bc9051e
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2012-11-04 12:40:35 +0100

    Ticket #70 Removed swt and ide plugins and features

--- de.lmu.ifi.dbs.knowing.core.swt.charts/src/de/lmu/ifi/dbs/knowing/core/swt/charts/events/ChartEvent.scala
+++
@@ -1,7 +0,0 @@
-package de.lmu.ifi.dbs.knowing.core.swt.charts.events
-
-import de.lmu.ifi.dbs.knowing.core.events.UIEvent
-import org.jfree.chart.event.{ChartProgressListener, ChartChangeListener }
-
-case class ChartProgressListenerRegister(listener: ChartProgressListener) extends UIEvent
-case class ChartChangeListenerRegister(listener: ChartChangeListener) extends UIEvent
